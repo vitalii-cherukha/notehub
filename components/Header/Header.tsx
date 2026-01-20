da50d27e99f7cb6c6e75c43e5cb1d5bdd577fa9b
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-01-20 13:03:09 +0200

    refactoring header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,19 +7,22 @@ import AuthNavigation from '../AuthNavigation/AuthNavigation';
 const Header = () => {
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home">
-        NoteHub
-      </Link>
-      <nav aria-label="Main Navigation">
-        <ul className={css.navigation}>
-          <li className={css.navigationItem}>
-            <Link className={css.navigationLink} href="/">
-              Home
-            </Link>
-          </li>
-          <AuthNavigation />
-        </ul>
-      </nav>
+      <div className={css.container}>
+        <Link href="/" className={css.logo} aria-label="Home">
+          <span className={css.logoIcon}>📝</span>
+          NoteHub
+        </Link>
+        <nav aria-label="Main Navigation">
+          <ul className={css.navigation}>
+            <li className={css.navigationItem}>
+              <Link className={css.navigationLink} href="/">
+                Home
+              </Link>
+            </li>
+            <AuthNavigation />
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
